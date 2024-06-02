f6951129aea94a86b1a4dc430c96e17f1c54e65c
Pavel <p.mirganov@yandex.ru>
2024-06-02 23:54:30 +0700

    Good

---
+++ src/Business/Abstract/Services/IOrderBlocksService.cs
@@ -0,0 +1,8 @@
+﻿namespace Forpost.Business.Abstract.Services
+{
+    public interface IOrderBlocksService : IBusinessService
+    {
+        public void GetOrder(int id, CancellationToken cancellationToken);
+
+    }
+}
---
+++ src/Business/Services/OrderBlocksService.cs
@@ -0,0 +1,17 @@
+﻿using Forpost.Business.Abstract.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forpost.Business.Services
+{
+    internal sealed class OrderBlocksService : IOrderBlocksService
+    {
+        public void GetOrder(int id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
