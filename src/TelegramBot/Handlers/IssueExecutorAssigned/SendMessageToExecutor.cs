f40b3396312e26b0263f2add53df0d190501bc35
Pavel <p.mirganov@yandex.ru>
2024-09-29 23:44:14 +0700

    **

---
+++ src/TelegramBot/Handlers/IssueExecutorAssigned/SendMessageToExecutor.cs
@@ -0,0 +1,34 @@
+using System.Text;
+using Forpost.Application.Contracts.ProductCreating.Issues;
+using Forpost.Domain.ProductCreating.Issue;
+using Forpost.Domain.ProductCreating.Issue.Events;
+using Mediator;
+using Telegram.Bot;
+
+namespace Forpost.TelegramBot.Handlers.IssueExecutorAssigned;
+
+public sealed class SendMessageToExecutor : INotificationHandler<ExecutorAssigned>, ITelegramBotSender
+{
+    private readonly ITelegramBotClient _botClient;
+    private readonly IIssueReadRepository _issueReadRepository;
+
+    public SendMessageToExecutor(ITelegramBotClient botClient, IIssueReadRepository issueReadRepository)
+    {
+        _botClient = botClient;
+        _issueReadRepository = issueReadRepository;
+    }
+
+    public async ValueTask Handle(ExecutorAssigned notification, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"{notification.ExecutorId} executor assigned");
+        var issue = await _issueReadRepository.GetById(notification.IssueId, cancellationToken);
+        string messageText = $"Вы назначены исполнителем задачи:\n" +
+                             $"Операция: {issue.OperationName}\n" +
+                             $"Продукт: {issue.ProductName}\n" +
+                             $"Целевое количество: {issue.TargetQuantity}\n" +
+                             $"Ответственный: {issue.ResponsibleName}";
+        
+        await _botClient.SendTextMessageAsync(1013271587, messageText,
+            cancellationToken: cancellationToken);
+    }
+}
---
+++ src/TelegramBot/TelegramBotAssemblyReference.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+namespace Forpost.TelegramBot;
+
+public class TelegramBotAssemblyReference
+{
+    public static Assembly Assembly => typeof(TelegramBotAssemblyReference).Assembly;
+}
