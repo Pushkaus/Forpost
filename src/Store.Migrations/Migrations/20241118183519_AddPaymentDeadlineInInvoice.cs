dc4ef53b5cb7fbef6d413fff0a50fb937e78299f
Pavel <p.mirganov@yandex.ru>
2024-11-19 12:58:38 +0700

    refactoring invoice and fix other

---
+++ src/Store.Migrations/Migrations/20241118183519_AddPaymentDeadlineInInvoice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Forpost.Store.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentDeadlineInInvoice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "PaymentDeadline",
+                table: "Invoices",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentDeadline",
+                table: "Invoices");
+        }
+    }
+}
