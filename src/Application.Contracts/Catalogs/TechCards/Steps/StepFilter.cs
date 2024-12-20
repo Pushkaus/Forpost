073094a9ff33643c23184890e02212ce4fef25a1
Pavel <p.mirganov@yandex.ru>
2024-12-20 14:07:52 +0700

    **

--- src/Application.Contracts/Catalogs/TechCards/Steps/StepFilter.cs
+++ src/Application.Contracts/Catalogs/TechCards/Steps/StepFilter.cs
@@ -3,6 +3,6 @@ namespace Forpost.Application.Contracts.Catalogs.TechCards.Steps;
 public sealed class StepFilter
 {
     public Guid? OperationId { get; set; }
-    public int Skip { get; set; } = 10;
+    public int Skip { get; set; } = 0;
     public int Limit { get; set; } = 10;
 }
