ae54760174bfb55b3c61a2862e647420e09eec60
Pavel <p.mirganov@yandex.ru>
2024-12-24 11:33:51 +0700

    **

--- src/Domain/Catalogs/TechCards/Operations/OperationType.cs
+++ src/Domain/Catalogs/TechCards/Operations/OperationType.cs
@@ -7,11 +7,11 @@ public sealed class OperationType: SmartEnumeration<OperationType>
     /// <summary>
     /// Базовая операция, где необходимо учесть количество продукта
     /// </summary>
-    public static readonly OperationType Basic = new(nameof(Basic), 0);
+    public static readonly OperationType Basic = new(nameof(Basic), 100);
     /// <summary>
     /// Подготовительная операция. Готовится сразу партия
     /// </summary>
-    public static readonly OperationType Preparatory = new(nameof(Preparatory), 100);
+    public static readonly OperationType Preparatory = new(nameof(Preparatory), 200);
     
     public OperationType(string name, int value) : base(name, value)
     {
